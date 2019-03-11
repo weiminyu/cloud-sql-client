d4119914ac0944bdd19fb5fc9bf019d9d7b8efb0
Weimin Yu <weiminyu@google.com>
2019-03-11 15:44:19 -0400

    Add hermetic connection pool tests for mysql

--- subprojects/jdbc/src/test/java/jdbc/connection/ConnectionPoolTest.java
+++ subprojects/jdbc/src/test/java/jdbc/connection/ConnectionPoolTest.java
@@ -6,39 +6,84 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import javax.sql.DataSource;
 import jdbc.connection.ConnectionConfigurations.DbType;
+import junitparams.JUnitParamsRunner;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 
-@RunWith(JUnit4.class)
+/**
+ * Unit tests (hermetic) for JDBC connection pools using local database instances started with
+ * Docker.
+ */
+@RunWith(JUnitParamsRunner.class)
 public class ConnectionPoolTest {
 
-  @ClassRule
-  public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:9.6");
-
-  private static DataSource dataSource;
-
   @BeforeClass
-  public static void setupJdbcDataSource() {
-    postgreSQLContainer.start();
-    dataSource =
-        HikariDataSources.createDataSource(
-            postgreSQLContainer.getUsername(),
-            postgreSQLContainer.getPassword(),
-            postgreSQLContainer.getJdbcUrl(),
-            DbType.PSQL.proxyProperties());
+  public static void setupDatabases() {
+    for (DataSourceFactory factory : DataSourceFactory.values()) {
+      factory.startDockerContainer();
+    }
+  }
+
+  @AfterClass
+  public static void tearDownClass() {
+    for (DataSourceFactory factory : DataSourceFactory.values()) {
+      factory.stopDockerContainer();
+    }
   }
 
   @Test
-  public void dummy() throws Exception {
-    try (Connection conn = dataSource.getConnection()) {
-      ResultSet resultSet = conn.createStatement().executeQuery("select version()");
+  @junitparams.Parameters
+  public void connect_emptyDb(
+      DataSourceFactory dataSourceFactory, String sql, int columnIndex, String resultPrefix)
+      throws Exception {
+    try (Connection conn = dataSourceFactory.get().getConnection()) {
+      ResultSet resultSet = conn.createStatement().executeQuery(sql);
       assertThat(resultSet.next()).isTrue();
-      assertThat(resultSet.getString(1)).startsWith("PostgreSQL 9.6");
+      assertThat(resultSet.getString(columnIndex)).startsWith(resultPrefix);
       assertThat(resultSet.next()).isFalse();
+    }
+  }
+
+  @SuppressWarnings("unused")
+  private Object[] parametersForConnect_emptyDb() {
+    return new Object[] {
+      new Object[] {DataSourceFactory.MYSQL, "show variables like 'version'", 2, ""},
+      new Object[] {DataSourceFactory.PSQL, "select version()", 1, "PostgreSQL 9.6.12"}
+    };
+  }
+
+  enum DataSourceFactory {
+    MYSQL(new MySQLContainer<>("mysql:5.7.25"), DbType.MYSQL),
+    PSQL(new PostgreSQLContainer<>("postgres:9.6.12"), DbType.PSQL);
+
+    private final JdbcDatabaseContainer<?> databaseContainer;
+    private final DbType dbType;
+
+    DataSourceFactory(JdbcDatabaseContainer<?> databaseContainer, DbType dbType) {
+      this.databaseContainer = databaseContainer;
+      this.dbType = dbType;
+    }
+
+    public void startDockerContainer() {
+      databaseContainer.start();
+    }
+
+    public void stopDockerContainer() {
+      databaseContainer.stop();
+    }
+
+    public DataSource get() {
+      databaseContainer.start();
+      return HikariDataSources.createDataSource(
+          databaseContainer.getUsername(),
+          databaseContainer.getPassword(),
+          databaseContainer.getJdbcUrl(),
+          dbType.proxyProperties());
     }
   }
 }
